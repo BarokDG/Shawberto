c4c867c500f4fa7e3bd2b3d7b43eb82963406ebf
BarokDG <it.barok.dagim@gmail.com>
2023-01-18 13:41:56 +0300

    Hide Environment variables, minor changes

--- bot.ts
+++ bot.ts
@@ -1,51 +1,63 @@
 import { Bot } from "grammy";
 import axios from "axios";
 
-const bot = new Bot("5898666265:AAHCrh13937amAcJiq_B8qRpCi_TBMZesNo");
+function main() {
+  if (!process.env.BOT_TOKEN) return;
 
-bot.hears(/^https:\/\/www.tiktok.com\/.*/g, (ctx) => {
-  if (!ctx.message?.text) return;
+  const bot = new Bot(process.env.BOT_TOKEN);
 
-  try {
-    fetchVideo(ctx.message.text).then((videoUrl: string | null) => {
-      if (!videoUrl) {
-        ctx.reply("Invalid url");
-        return;
-      }
+  // listen to tiktok links
+  bot.hears(/^https:\/\/www.tiktok.com\/.*/g, (ctx) => {
+    if (!ctx.message?.text) return;
 
-      ctx.replyWithVideo(videoUrl);
-    });
-  } catch (err) {
-    ctx.reply("This shouldn't happen. Call Barok.");
+    try {
+      fetchTiktokVideo(ctx.message.text).then((videoUrl: string | null) => {
+        if (!videoUrl) {
+          ctx.reply("Invalid url");
+          return;
+        }
+
+        ctx.replyWithVideo(videoUrl);
+      });
+    } catch (err) {
+      ctx.reply("This shouldn't happen. Call Barok.");
+    }
+  });
+
+  // To test if running
+  bot.hears(/Shawberto, you good?/, (ctx) =>
+    ctx.reply("Shawberto is running.")
+  );
+
+  bot.start();
+
+  async function fetchTiktokVideo(link: string | null): Promise<string | null> {
+    const options = {
+      method: "GET",
+      url: "https://tiktok-downloader-download-tiktok-videos-without-watermark.p.rapidapi.com/vid/index",
+      params: {
+        url: link,
+      },
+      headers: {
+        "X-RapidAPI-Key": "11a08a8211msh8578bdf05a9ed95p11e117jsnd72bdf0d38fe",
+        "X-RapidAPI-Host":
+          "tiktok-downloader-download-tiktok-videos-without-watermark.p.rapidapi.com",
+      },
+    };
+
+    let tiktokCDNLink: string | null = null;
+
+    await axios
+      .request(options)
+      .then(function (response: { data: any }) {
+        tiktokCDNLink = response.data.video[0];
+      })
+      .catch(function (error: any) {
+        console.error(error);
+      });
+
+    return tiktokCDNLink;
   }
-});
-
-bot.start();
-
-async function fetchVideo(link: string | null) {
-  const options = {
-    method: "GET",
-    url: "https://tiktok-downloader-download-tiktok-videos-without-watermark.p.rapidapi.com/vid/index",
-    params: {
-      url: link,
-    },
-    headers: {
-      "X-RapidAPI-Key": "11a08a8211msh8578bdf05a9ed95p11e117jsnd72bdf0d38fe",
-      "X-RapidAPI-Host":
-        "tiktok-downloader-download-tiktok-videos-without-watermark.p.rapidapi.com",
-    },
-  };
-
-  let tiktokCDNLink = null;
-
-  await axios
-    .request(options)
-    .then(function (response: { data: any }) {
-      tiktokCDNLink = response.data.video[0];
-    })
-    .catch(function (error: any) {
-      console.error(error);
-    });
-
-  return tiktokCDNLink;
 }
+
+main();
