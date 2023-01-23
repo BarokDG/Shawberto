ac8b7695d0544b4f5ba9abd408d85cf0a1226c27
BarokDG <it.barok.dagim@gmail.com>
2023-01-23 16:17:01 +0300

    Add script to setup webhook

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Bot } from "grammy";
+import axios from "axios";
 
 require("dotenv").config();
 
@@ -8,6 +8,30 @@ if (!process.env.BOT_TOKEN)
 if (!process.env.WEBHOOK_ADDRESS)
   throw new Error("Environemnt variable 'WEBHOOK_ADDRESS' not set");
 
-const bot = new Bot(process.env.BOT_TOKEN);
+(async function () {
+  let isWebhookSet = false;
 
-bot.api.setWebhook(process.env.WEBHOOK_ADDRESS);
+  let res = await axios.request({
+    method: "GET",
+    url: `https://api.telegram.org/bot${process.env.BOT_TOKEN}/getWebhookInfo`,
+  });
+
+  isWebhookSet = res.data.result.url === process.env.WEBHOOK_ADDRESS;
+
+  if (isWebhookSet) return;
+
+  axios
+    .request({
+      method: "POST",
+      url: `https://api.telegram.org/bot${process.env.BOT_TOKEN}/setWebhook`,
+      params: {
+        url: process.env.WEBHOOK_ADDRESS,
+      },
+    })
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+})();
