3156b7f9506b428b2ed53951f0f728cb119df1ac
Barok Dagim <barok@garrilogistics.com>
2023-09-20 08:33:03 +0300

    Starter files and deployment setup

---
+++ api/bot.ts
@@ -0,0 +1,8 @@
+import { Bot, webhookCallback } from "grammy";
+
+const token = process.env.BOT_TOKEN;
+if (!token) throw new Error("BOT_TOKEN is unset");
+
+const bot = new Bot(token);
+
+export default webhookCallback(bot, "http");
