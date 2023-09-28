83bff5a1b74fbfe84d8990c8a0afcf4fd9c1df8a
Barok Dagim <barok@garrilogistics.com>
2023-09-28 11:48:36 +0300

    refactor: clean up bot.ts

--- api/bot.ts
+++ api/bot.ts
@@ -1,16 +1,25 @@
 import {
   type Context,
+  type HearsContext,
   type NextFunction,
   type Transformer,
   Bot,
   webhookCallback,
 } from "grammy";
+import type { Message } from "grammy/types";
 import { autoRetry } from "@grammyjs/auto-retry";
 import * as Sentry from "@sentry/node";
 import "dotenv/config";
 
 import { getTiktokVideoInfo, getTweetInfo } from "../src/services";
 import type { TikTokVideoInfo, TweetInfo } from "../src/types";
+import {
+  TIKTOK_LINK_REGEX,
+  TWITTER_LINK_REGEX,
+  SHAWBERTO_REGEX,
+  DEVBERTO_REGEX,
+  SECONDS_TO_SHOW_ERROR_BEFORE_DELETING,
+} from "../src/constants";
 
 const { BOT_TOKEN, ENV, SENTRY_DSN } = process.env;
 
@@ -19,11 +28,6 @@ Sentry.init({
 });
 
 const isDevelopment = ENV === "development";
-
-const TIKTOK_LINK_REGEX = /^https:\/\/(www|vm|vt).tiktok.com\/.*/g;
-const TWITTER_LINK_REGEX = /^https:\/\/(www\.)?(twitter|x).com\/.*/g;
-const SHAWBERTO_REGEX = /Shawberto, you good?/g;
-const DEVBERTO_REGEX = /Devberto, you good?/g;
 
 if (!BOT_TOKEN) throw new Error("BOT_TOKEN is unset");
 
@@ -37,8 +41,55 @@ bot.command(
   async (ctx) => await ctx.reply("Welcome! Up and running.")
 );
 
-// Handle TikTok links
-bot.on("::url").hears(TIKTOK_LINK_REGEX, async (ctx) => {
+bot.on("::url").hears(TIKTOK_LINK_REGEX, handleTikTokLink);
+bot.on("::url").hears(TWITTER_LINK_REGEX, handleTwitterLink);
+
+bot
+  .on(":text")
+  .hears(
+    isDevelopment ? DEVBERTO_REGEX : SHAWBERTO_REGEX,
+    async (ctx) => await ctx.reply(`${ctx.me.first_name} is running`)
+  );
+
+if (isDevelopment) {
+  void bot.start({
+    drop_pending_updates: true,
+  });
+}
+
+export default webhookCallback(bot, "next-js");
+
+// Use telegram's reply to message feature for all messages. See https://telegram.org/blog/replies-mentions-hashtags?setln=en#replies
+function replyToMessageTransformer(ctx: Context): Transformer {
+  const transformer: Transformer = async (prev, method, payload, signal) => {
+    if (!method.startsWith("send")) {
+      return await prev(method, payload, signal);
+    }
+
+    if (ctx.chat?.type === "private") {
+      return await prev(method, payload, signal);
+    }
+
+    return await prev(
+      method,
+      { ...payload, reply_to_message_id: ctx.message?.message_id },
+      signal
+    );
+  };
+
+  return transformer;
+}
+
+// Need middleware to provide context to the transformer
+async function replyToMessageMiddleWare(
+  ctx: Context,
+  next: NextFunction
+): Promise<void> {
+  ctx.api.config.use(replyToMessageTransformer(ctx));
+  await next();
+}
+
+async function handleTikTokLink(ctx: HearsContext<Context>): Promise<void> {
   if (!ctx.message?.text) return;
 
   const loader = await ctx.reply("Processing link...");
@@ -65,23 +116,11 @@ bot.on("::url").hears(TIKTOK_LINK_REGEX, async (ctx) => {
 
     await bot.api.deleteMessage(ctx.chat.id, loader.message_id);
   } catch (error) {
-    Sentry.captureException(error);
-
-    await bot.api.editMessageText(
-      ctx.chat.id,
-      loader.message_id,
-      "Error processing link"
-    );
-
-    const timer = setTimeout(() => {
-      void bot.api.deleteMessage(ctx.chat.id, loader.message_id);
-      clearTimeout(timer);
-    }, 1000);
+    await handleError(error, ctx, loader);
   }
-});
+}
 
-// Handle Twitter links
-bot.on("::url").hears(TWITTER_LINK_REGEX, async (ctx) => {
+async function handleTwitterLink(ctx: HearsContext<Context>): Promise<void> {
   if (!ctx.message?.text) return;
 
   const loader = await ctx.reply("Processing link...");
@@ -128,63 +167,26 @@ bot.on("::url").hears(TWITTER_LINK_REGEX, async (ctx) => {
 
     await bot.api.deleteMessage(ctx.chat.id, loader.message_id);
   } catch (error) {
-    Sentry.captureException(error);
-
-    await bot.api.editMessageText(
-      ctx.chat.id,
-      loader.message_id,
-      "Error processing link"
-    );
-
-    const timer = setTimeout(() => {
-      void bot.api.deleteMessage(ctx.chat.id, loader.message_id);
-      clearTimeout(timer);
-    }, 1000);
+    await handleError(error, ctx, loader);
   }
-});
+}
 
-// Test if bot is running
-bot
-  .on(":text")
-  .hears(
-    isDevelopment ? DEVBERTO_REGEX : SHAWBERTO_REGEX,
-    async (ctx) => await ctx.reply(`${ctx.me.first_name} is running`)
+async function handleError(
+  error: unknown,
+  ctx: Context,
+  loader: Message
+): Promise<void> {
+  Sentry.captureException(error);
+
+  await bot.api.editMessageText(
+    ctx.chat?.id as number,
+    loader.message_id,
+    "Error processing link"
   );
 
-if (isDevelopment) {
-  void bot.start({
-    drop_pending_updates: true,
-  });
-}
+  await new Promise((resolve) =>
+    setTimeout(resolve, SECONDS_TO_SHOW_ERROR_BEFORE_DELETING)
+  );
 
-export default webhookCallback(bot, "next-js");
-
-// Use telegram's reply to message feature for all messages. See https://telegram.org/blog/replies-mentions-hashtags?setln=en#replies
-function replyToMessageTransformer(ctx: Context): Transformer {
-  const transformer: Transformer = async (prev, method, payload, signal) => {
-    if (!method.startsWith("send")) {
-      return await prev(method, payload, signal);
-    }
-
-    if (ctx.chat?.type === "private") {
-      return await prev(method, payload, signal);
-    }
-
-    return await prev(
-      method,
-      { ...payload, reply_to_message_id: ctx.message?.message_id },
-      signal
-    );
-  };
-
-  return transformer;
-}
-
-// Need middleware to provide context to the transformer
-async function replyToMessageMiddleWare(
-  ctx: Context,
-  next: NextFunction
-): Promise<void> {
-  ctx.api.config.use(replyToMessageTransformer(ctx));
-  await next();
+  await bot.api.deleteMessage(ctx.chat?.id as number, loader.message_id);
 }
---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const TIKTOK_LINK_REGEX = /^https:\/\/(www|vm|vt).tiktok.com\/.*/g;
+export const TWITTER_LINK_REGEX = /^https:\/\/(www\.)?(twitter|x).com\/.*/g;
+export const SHAWBERTO_REGEX = /Shawberto, you good?/g;
+export const DEVBERTO_REGEX = /Devberto, you good?/g;
+
+export const SECONDS_TO_SHOW_ERROR_BEFORE_DELETING = 1000;
