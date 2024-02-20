1700950f75a8caf9568dc8449c672e88aa4fc2ab
Barok Dagim <dagimbarok@gmail.com>
2024-02-20 20:03:11 +0300

    fix: regex issues

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,9 @@
-export const TIKTOK_LINK_REGEX = /(https:\/\/)?(www|vm|vt)\.tiktok\.com\/t\/[a-zA-Z0-9_-]+\//;
-export const TWITTER_LINK_REGEX = /(https:\/\/)?(www\.)?(twitter|x).com\/[a-zA-Z0-9_-]+\//;
-export const INSTAGRAM_REEL_LINK_REGEX = /(https:\/\/)?(www\.)?(instagram).com\/reel\/[a-zA-Z0-9_-]+\//;
+export const TIKTOK_LINK_REGEX =
+  /(https:\/\/)?(www|vm|vt)\.tiktok\.com\/t\/[a-zA-Z0-9_-]+\/?/;
+export const TWITTER_LINK_REGEX =
+  /(https:\/\/)?(www\.)?(twitter|x).com\/[a-zA-Z0-9_-]+\/?/;
+export const INSTAGRAM_REEL_LINK_REGEX =
+  /(https:\/\/)?(www\.)?instagram.com\/reel\/[a-zA-Z0-9_-]+\/?/;
 export const SHAWBERTO_REGEX = /Shawberto, you good?/;
 export const DEVBERTO_REGEX = /Devberto, you good?/;
 
